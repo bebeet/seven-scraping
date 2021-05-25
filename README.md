8f4adae637d30e1dc4288265276e988413ef58f0
Punsiri Boonyakiat <punsiri.boo@gmail.com>
2021-05-26 05:35:04 +0700

    first visualization

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # seven-scraping
 Web Scraping for 7-eleven and Visualisation
+
+## Requirement
+curl -O https://raw.githubusercontent.com/apisit/thailand.json/master/thailandWithName.json
+
+## How to run
+```
+pip install -r requirements.txt
+python scraper/seven_scraper.py
+```
+
+
+## Reference
+* https://www.facebook.com/groups/colab.thailand/permalink/1421960354645985/
--- scraper/seven_scraper.py
+++ scraper/seven_scraper.py
@@ -14,11 +14,23 @@ def get_seven_store(lat:float, long:float):
     json_data = response.json()
     return json_data
 
+def get_seven_store_by_keyword(keyword:str):
+    """ Reuest api with post method to 7elven     
+        Arguments: lat, long
+        Returns: json_data
+    """
+    url = "https://www.7eleven.co.th/api/v1/Store/GetStoreBySearch"
+    payload = {"keyword": keyword} 
+    headers = {'Accept': 'application/json, text/plain, */*'}
+    response = requests.post(url, headers=headers, data=payload)
+    json_data = response.json()
+    return json_data
+
 """ Read tambon df and use TA_ID to scrape location based on Thai Tambon"""
 selected_province_list = ["กรุงเทพมหานคร", "จ. สมุทรปราการ", "จ. นนทบุรี", "จ. ปทุมธานี", "จ. นครปฐม", "จ. สมุทรสงคราม"]
 tambon_lat_long_df = pd.read_excel('kh-muulphikad-lat-long-thiibngchiichuue-tambl-ameph-cchanghwad.xlsx')  
 tambon_lat_long_df = tambon_lat_long_df[tambon_lat_long_df.CHANGWAT_T.isin(selected_province_list)]
-tambon_lat_long_df = tambon_lat_long_df[["TA_ID", "LAT", "LONG"]]
+tambon_lat_long_df = tambon_lat_long_df[["TA_ID", "TAMBON_T", "LAT", "LONG"]]
 tambon_lat_long_df = tambon_lat_long_df.drop_duplicates(subset='TA_ID', keep="first")
 tambon_lat_long_df = tambon_lat_long_df.sort_values(by=['TA_ID'])
 tambon_lat_long_df = tambon_lat_long_df.reset_index()
@@ -27,9 +39,16 @@ print(tambon_lat_long_df.head())
 RESULT_DIRECTORY = "raw-data"
 print("Total api calls:", len(tambon_lat_long_df))
 for index, row in tambon_lat_long_df.iterrows():
-    print(index, row['TA_ID'], row['LAT'], row["LONG"])
-    filename = f"{RESULT_DIRECTORY}/{index}_{int(row['TA_ID'])}.json"
+    print(index, row['TA_ID'], row['TAMBON_T'], row['LAT'], row["LONG"])
+
+    json_data = get_seven_store_by_keyword(row['TAMBON_T'].replace(" ",""))
+    filename = f"{RESULT_DIRECTORY}/by_province_name/{index}_{int(row['TA_ID'])}.json"
+    with open(filename, 'w') as outfile: json.dump(json_data, outfile, ensure_ascii=False)
+    sleep_time = random.randint(30, 90)
+    time.sleep(sleep_time)
+
     json_data = get_seven_store(row['LAT'], row['LONG'])
+    filename = f"{RESULT_DIRECTORY}/by_latlng/{index}_{int(row['TA_ID'])}.json"
     with open(filename, 'w') as outfile: json.dump(json_data, outfile, ensure_ascii=False)
     sleep_time = random.randint(30, 90)
     time.sleep(sleep_time)
